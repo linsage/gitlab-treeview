ff29228f8bc0b3877ef698bf7fc7885094c65ac0
林静杰 <linsage@live.com>
2017-07-18 16:00:00 +0800

    停留样式

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var gitlabTree = {　　　　
+var vm = {　　　　
     /* api variables */
     private_token: null,
     apiRootUrl: null,
@@ -15,29 +15,56 @@ var gitlabTree = {　　　　
             return true;
         }
     },
+    isFilePage: function () {
+        return $(".tree-item-file-name").size() > 0;
+    },
     initVariables: function () {
         var href = "" + $("head link[rel='alternate']").attr("href");
         var index = href.indexOf("=");
         if (index > -1) {
-            this.private_token = href.substring(index + 1);
+            vm.private_token = href.substring(index + 1);
         }
-        this.apiRootUrl = window.location.origin;
-        this.project_id = $('#project_id').val() || $('#search_project_id').val();
-        this.apiProjects = this.apiRootUrl + '/api/v3/projects/';
-        this.apiRepoTree = this.apiProjects + this.project_id + '/repository/tree';
-        this.repository_ref = $('#repository_ref').val();
-        console.info(this)
+        vm.apiRootUrl = window.location.origin;
+        vm.project_id = $('#project_id').val() || $('#search_project_id').val();
+        vm.apiProjects = vm.apiRootUrl + '/api/v3/projects/';
+        vm.apiRepoTree = vm.apiProjects + vm.project_id + '/repository/tree';
+        vm.repository_ref = $('#repository_ref').val();
+        console.info(vm)
     },
-    init: function () {
-        if (!this.isGitLab()) {
-            return;
-        }
-
-        this.initVariables();
-
-        var $tree = $("<ul class='ztree'></ul>");
-        $("body").append($tree);
-
+    getApiRepoTree: function (path) {
+        $.get(vm.apiRepoTree, {
+            id: vm.project_id,
+            path: path,
+            ref_name: vm.repository_ref,
+            private_token: vm.private_token
+        }, function (result) {
+            console.info(result);
+        });
+    },
+    getApiProjects: function () {
+        $.get(vm.apiProjects, {
+            private_token: ''
+        }, function (result) {
+            console.info(result);
+        });
+    },
+    showTree: function () {
+        $("html").css("margin-left", "230px");
+        $(".gitlabTreeView_sidebar").animate({
+            "width": "230px"
+        }, 'fast', "linear", function () {
+            $(".gitlabTreeView_toggle i").removeClass().addClass("fa fa-arrow-left");
+        });
+    },
+    hideTree: function () {
+        $("html").css("margin-left", "0px");
+        $(".gitlabTreeView_sidebar").animate({
+            "width": "0px"
+        }, 'fast', "linear", function () {
+            $(".gitlabTreeView_toggle i").removeClass().addClass("fa fa-arrow-right");
+        });
+    },
+    initTree: function () {
         var setting = {
             view: {
                 showLine: false
@@ -49,12 +76,45 @@ var gitlabTree = {　　　　
             }
         };
 
-        var zNodes = [];
-        $.fn.zTree.init($(".ztree"), setting, zNodes);
+        $.fn.zTree.init($(".ztree"), setting);
+    },
+    init: function () {
+        if (!vm.isGitLab() || !vm.isFilePage()) {
+            return;
+        }
+
+        vm.initVariables();
+
+        var shortcuts_project = "" + $(".shortcuts-project").attr("href");
+
+        shortcuts_project = shortcuts_project.substring(1).replace("/", " / ");
+
+        var nav = "<nav class='gitlabTreeView_sidebar'>";
+        nav += "<a class='gitlabTreeView_toggle'><i class='fa fa-arrow-left'></i></a>";
+        nav += "<div class='gitlabTreeView_content'>";
+        nav += "<div class='gitlabTreeView_header'>";
+        nav += "<div class='gitlabTreeView_header_repo'><i class='fa fa-gitlab gitlabTreeView_tab'></i>" + shortcuts_project + "</div>";
+        nav += "<div class='gitlabTreeView_header_branch'><i class='fa fa-share-alt gitlabTreeView_tab'></i>" + vm.repository_ref + "</div>";
+        nav += "</div>";
+        nav += "<div class='gitlabTreeView_body'><ul class='ztree'></ul></div>";
+        nav += "</div>";
+        nav += "</div>";
+        $("body").append($(nav));
+        vm.hideTree();
+
+        $(".gitlabTreeView_toggle").on('click', function () {
+            if ($(".gitlabTreeView_sidebar").width() > 0) {
+                vm.hideTree();
+            } else {
+                vm.showTree();
+            }
+        });
+
+        vm.getApiRepoTree('/');
     }
 };
 
 
 $(function () {
-    gitlabTree.init();
+    vm.init();
 });
