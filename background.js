0286aca69c75f0d1ec0d8fc63378a76784b93af7
zWingz <zhengzwing@gmail.com>
2019-05-09 10:52:40 +0800

    docs: update info

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 // current tab.
 function openSupport(tab) {
-    window.open("https://chrome.google.com/webstore/detail/gitlab-treeview/" + chrome.runtime.id + "/support");
+    // window.open("https://chrome.google.com/webstore/detail/gitlab-treeview-fork/" + chrome.runtime.id + "/support");
+    window.open("https://github.com/zWingz/gitlab-treeview")
 }
 
 // When the browser action is clicked, call the
